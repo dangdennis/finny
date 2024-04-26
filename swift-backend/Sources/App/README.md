18f8a662581f87d5a0e702a36ea7d82ad6388b96
Dennis Dang <dangggdennis@gmail.com>
2024-04-25 23:58:15 -0400

    add swift-format to makefile

--- swift-backend/Sources/App/README.md
+++ swift-backend/Sources/App/README.md
@@ -1,5 +1,8 @@
 # Financial Planner
 
 ## Getting started
+- `brew install swift-format`
 - `docker compose up`
 - `swift build`
+
+See Makefile for all development aliases.
--- swift-backend/Sources/App/routes.swift
+++ swift-backend/Sources/App/routes.swift
@@ -2,17 +2,17 @@ import Fluent
 import Vapor
 
 func routes(_ app: Application) throws {
-    app.get { req async throws in
-        try await req.view.render("index", ["title": "Hello Vapor!"])
-    }
+  app.get { req async throws in
+    try await req.view.render("index", ["title": "Hello Vapor!"])
+  }
 
-    app.get("hello") { req async -> String in
-        "Hello, world!"
-    }
+  app.get("hello") { req async -> String in
+    "Hello, world!"
+  }
 
-     app.get("okay") { req async -> String in
-        "Hello, world!"
-    }
+  app.get("okay") { req async -> String in
+    "Hello, world!"
+  }
 
-    try app.register(collection: TodoController())
+  try app.register(collection: TodoController())
 }
