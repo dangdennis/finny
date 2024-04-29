4af040f076d04095c8f7ce2e427bbda3ac71d1b4
Dennis Dang <dangggdennis@gmail.com>
2024-04-28 23:45:03 -0400

    rename new to register route

--- swift-backend/Sources/App/routes.swift
+++ swift-backend/Sources/App/routes.swift
@@ -17,7 +17,7 @@ func routes(_ app: Application) throws {
 
   app.group("api") { api in
     api.group("users") { users in
-      users.post("new") { req async throws in
+      users.post("register") { req async throws in
         return try await userController.create(req: req)
       }
       users.post("login") { req async throws in
