016e916255b1c5ecfb43f95504a6ea87513ebf0b
Dennis Dang <dangggdennis@gmail.com>
2024-05-05 14:10:21 -0400

    idempotency migration

--- swift-backend/Sources/App/Migrations/202405051342-PowerSyncPublications.swift
+++ swift-backend/Sources/App/Migrations/202405051342-PowerSyncPublications.swift
@@ -7,13 +7,19 @@ extension PowerSync {
             try await (database as! SQLDatabase).raw(
                 """
                 -- Create publication for powersync
-                create publication powersync for table 
-                    public.\(unsafeRaw: Account.schema),
-                    public.\(unsafeRaw: Asset.schema),
-                    public.\(unsafeRaw: PlaidItem.schema),
-                    public.\(unsafeRaw: Transaction.schema),
-                    public.\(unsafeRaw: Goal.schema),
-                    public.\(unsafeRaw: User.schema);
+                DO $$
+                BEGIN
+                    -- Check if the publication already exists
+                    IF NOT EXISTS (SELECT 1 FROM pg_publication WHERE pubname = 'powersync') THEN
+                        create publication if not exists powersync for table 
+                            public.\(unsafeRaw: Account.schema),
+                            public.\(unsafeRaw: Asset.schema),
+                            public.\(unsafeRaw: PlaidItem.schema),
+                            public.\(unsafeRaw: Transaction.schema),
+                            public.\(unsafeRaw: Goal.schema),
+                            public.\(unsafeRaw: User.schema);
+                    END IF;
+                END $$;
                 """
             ).run()
         }
