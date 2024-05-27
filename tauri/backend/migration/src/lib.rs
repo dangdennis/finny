66cbca479bb7248bb139398983b7fe1761cb3e9d
Dennis Dang <dangggdennis@gmail.com>
2024-05-27 00:06:16 -0400

    rename migration file

--- tauri/backend/migration/src/lib.rs
+++ tauri/backend/migration/src/lib.rs
@@ -1,12 +1,12 @@
 pub use sea_orm_migration::prelude::*;
 
-mod m20240527_034542_init;
+mod m20240527_034542_users;
 
 pub struct Migrator;
 
 #[async_trait::async_trait]
 impl MigratorTrait for Migrator {
     fn migrations() -> Vec<Box<dyn MigrationTrait>> {
-        vec![Box::new(m20240527_034542_init::Migration)]
+        vec![Box::new(m20240527_034542_users::Migration)]
     }
 }
