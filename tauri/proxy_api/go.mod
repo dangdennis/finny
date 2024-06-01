84b22c77ac65d871d5f9945a6ec05a7f15654385
Dennis Dang <dangggdennis@gmail.com>
2024-06-01 16:30:14 -0400

    who cares about security, parse jwt, get sync status

--- tauri/proxy_api/go.mod
+++ tauri/proxy_api/go.mod
@@ -11,7 +11,9 @@ require (
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-cmp v0.6.0 // indirect
+	github.com/jmoiron/sqlx v1.4.0 // indirect
 	github.com/labstack/gommon v0.4.2 // indirect
+	github.com/lib/pq v1.10.9
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.20 // indirect
 	github.com/stretchr/testify v1.9.0 // indirect
--- tauri/proxy_api/main.go
+++ tauri/proxy_api/main.go
@@ -2,26 +2,63 @@ package main
 
 import (
 	"context"
+	"database/sql"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
 	"os"
+	"time"
 
 	// "github.com/joho/godotenv"
+	"github.com/golang-jwt/jwt"
+	"github.com/jmoiron/sqlx"
 	"github.com/plaid/plaid-go/v25/plaid"
 
 	"github.com/labstack/echo/v4"
 	"github.com/labstack/echo/v4/middleware"
+
+	_ "github.com/lib/pq"
 )
 
 // Options for the CLI. Pass `--port` or set the `SERVICE_PORT` env var.
 type Options struct {
-	Port int `help:"Port to listen on" short:"p" default:"8080"`
+	Port int `help:"Port to listen on" short:"p" default:"4000"`
 }
 
 type RequestBody struct {
 	AccessToken string `json:"access_token"`
+}
+
+const SYNC_STATUS_TABLE = `
+CREATE TABLE IF NOT EXISTS sync_statuses (
+    user_id uuid REFERENCES auth.users(id) PRIMARY KEY,
+    sync_seq int not null default 0,
+    last_sync timestamp
+);
+
+CREATE INDEX IF NOT EXISTS "ix:sync_statuses.id" ON sync_statuses(user_id uuid_ops);
+`
+
+const DATABASE_URL = "user=postgres.tqonkxhrucymdyndpjzf password=I07R6V4POCTi5wd4 host=aws-0-us-east-1.pooler.supabase.com port=6543 dbname=postgres"
+
+const SUPABASE_JWT_SECRET = "09sUFObcLZHvtRvj5LBqtQomVPuVqOAa/LW2hcdQqyxCwpH9JDOGPwmn6XHMpaxqUPfRWkxTgiB9i4rb1Vwxwg=="
+
+type SyncStatus struct {
+	UserID   string    `db:"user_id"`
+	SyncSeq  int32     `db:"sync_seq"`
+	LastSync time.Time `db:"last_sync"`
+}
+
+type SyncStatusDto struct {
+	UserID   string    `json:"user_id"`
+	SyncSeq  int32     `json:"sync_seq" `
+	LastSync time.Time `json:"last_sync"`
+}
+
+type User struct {
+	ID    string `db:"id"`
+	Email string `db:"email"`
 }
 
 func main() {
@@ -55,10 +92,77 @@ func main() {
 	configuration.AddDefaultHeader("PLAID-CLIENT-ID", plaidClientID)
 	configuration.AddDefaultHeader("PLAID-SECRET", plaidSecretString)
 	configuration.UseEnvironment(plaid.Sandbox)
+
+	db, err := sqlx.Connect("postgres", DATABASE_URL)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	db.MustExec(SYNC_STATUS_TABLE)
+
 	plaidClient := plaid.NewAPIClient(configuration)
 
 	e := echo.New()
 	e.Use(middleware.Logger())
+
+	e.GET("/internal/sync/status", func(c echo.Context) error {
+		token, err := jwt.Parse("eyJhbGciOiJIUzI1NiIsImtpZCI6IlFtdEZUekozSEd4T3ZzL1giLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzE3MzU5NDkzLCJpYXQiOjE3MTcyNzMwOTMsImlzcyI6Imh0dHBzOi8vdHFvbmt4aHJ1Y3ltZHluZHBqemYuc3VwYWJhc2UuY28vYXV0aC92MSIsInN1YiI6IjNiOTI0ZjM2LTk0ODEtNGVmOC1iNWVlLTM2OGJkYjIyNmUxNCIsImVtYWlsIjoiZGFuZ2dnZGVubmlzQGdtYWlsLmNvbSIsInBob25lIjoiIiwiYXBwX21ldGFkYXRhIjp7InByb3ZpZGVyIjoiZW1haWwiLCJwcm92aWRlcnMiOlsiZW1haWwiXX0sInVzZXJfbWV0YWRhdGEiOnt9LCJyb2xlIjoiYXV0aGVudGljYXRlZCIsImFhbCI6ImFhbDEiLCJhbXIiOlt7Im1ldGhvZCI6Im90cCIsInRpbWVzdGFtcCI6MTcxNzI3MzA5M31dLCJzZXNzaW9uX2lkIjoiMmJjZGY1NmUtNDkyYy00OGNmLTgxYmMtNGMxYmEwOWU4MzgzIiwiaXNfYW5vbnltb3VzIjpmYWxzZX0.myfAyjddlcIe_RtwYVzf0jYXvfehC7b_AVa0Rzn1IRI&expires_at=1717359493&expires_in=86400&refresh_token=UIQFTV-fSMT0IvWgySutkQ&token_type=bearer&type=magiclink", func(token *jwt.Token) (interface{}, error) {
+			// Validate the algorithm used to sign the token matches the expected algorithm
+			if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+				return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+			}
+			return SUPABASE_JWT_SECRET, nil
+		})
+
+		fmt.Println(token)
+
+		email := token.Claims.(jwt.MapClaims)["email"]
+
+		fmt.Println("email", email)
+
+		user := User{}
+		err = db.Get(&user, "SELECT id, email FROM auth.users WHERE email=$1", email)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				return c.JSON(http.StatusNotFound,
+					map[string]string{"error": "User not found"},
+				)
+			}
+			log.Println(err)
+			return c.JSON(http.StatusBadRequest,
+				map[string]string{"error": "Failed to fetch user"},
+			)
+		}
+
+		fmt.Println("User: ", user)
+
+		syncStatus := SyncStatus{}
+		err = db.Get(&syncStatus, "SELECT user_id, sync_seq, last_sync FROM sync_statuses WHERE user_id=$1", user.ID)
+		if err != nil {
+			if err == sql.ErrNoRows {
+				db.Exec("INSERT INTO sync_statuses (user_id, sync_seq, last_sync) VALUES ($1, 0, $2)", user.ID, time.Now())
+
+				return c.JSON(http.StatusNotFound,
+					map[string]string{"error": "Sync status not found"},
+				)
+			} else {
+				log.Println(err)
+				return c.JSON(http.StatusBadRequest,
+					map[string]string{"error": "Failed to fetch sync status"},
+				)
+			}
+		}
+
+		syncStatusDto := SyncStatusDto{
+			UserID:   syncStatus.UserID,
+			SyncSeq:  syncStatus.SyncSeq,
+			LastSync: syncStatus.LastSync,
+		}
+
+		fmt.Printf("%#v\n", syncStatusDto)
+
+		return c.JSON(200, syncStatusDto)
+	})
 
 	e.POST("/transactions/sync", func(c echo.Context) error {
 		ctx := context.Background()
