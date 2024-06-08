5d362735db4fb7cd35af499df773d74796e08574
Dennis Dang <dangggdennis@gmail.com>
2024-06-08 01:05:11 -0400

    dont let io input stop server in prod

--- finny-api/src/main/scala/com/belmont/Main.scala
+++ finny-api/src/main/scala/com/belmont/Main.scala
@@ -9,13 +9,23 @@ import sttp.tapir.server.netty.sync.{NettySyncServer, NettySyncServerOptions}
       .metricsInterceptor(Endpoints.prometheusMetrics.metricsInterceptor())
       .options
 
+    var appEnv = sys.env.getOrElse("APP_ENV", "development")
     val port = sys.env.get("HTTP_PORT").flatMap(_.toIntOption).getOrElse(8080)
 
     supervised {
       val binding = useInScope(NettySyncServer(serverOptions).port(port).addEndpoints(Endpoints.all).start())(_.stop())
       println(s"Go to http://localhost:${binding.port}/docs to open SwaggerUI.")
-      println(s"Press ENTER to stop the server.")
-      scala.io.StdIn.readLine()
+
+      appEnv match {
+        case "development" =>
+          println(s"Running in development mode.")
+          println(s"Press ENTER to stop the server.")
+          scala.io.StdIn.readLine()
+        case "production" =>
+          println(s"Running in production mode.")
+        case _ =>
+          println(s"Running in unknown mode.")
+      }
     }
   }
 
