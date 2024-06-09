502b6dc37d23396f062a170087023b532ace60d1
Dennis Dang <dangggdennis@gmail.com>
2024-06-09 03:59:22 -0400

    fix tests

--- finny-api/app/test/src/EndpointsSpec.scala
+++ finny-api/app/test/src/EndpointsSpec.scala
@@ -1,46 +1,27 @@
 
 package app
 
-import app.Endpoints.{*, given}
+import app.Endpoints._
 import org.scalatest.EitherValues
 import org.scalatest.flatspec.AnyFlatSpec
 import org.scalatest.matchers.should.Matchers
-import sttp.client3.*
+import sttp.client3._
 import sttp.client3.testing.SttpBackendStub
 import sttp.tapir.server.stub.TapirStubInterpreter
 
-import Library.*
-import sttp.client3.upicklejson.*
-
 class EndpointsSpec extends AnyFlatSpec with Matchers with EitherValues:
 
-  it should "return hello message" in {
+  it should "return hello world" in {
     // given
     val backendStub = TapirStubInterpreter(SttpBackendStub.synchronous)
-      .whenServerEndpointRunLogic(helloServerEndpoint)
+      .whenServerEndpointRunLogic(indexServerEndpoint)
       .backend()
 
     // when
     val response = basicRequest
-      .get(uri"http://test.com/hello?name=adam")
+      .get(uri"http://test.com/")
       .send(backendStub)
 
     // then
-    response.body.value shouldBe "Hello adam"
-  }
-
-  it should "list available books" in {
-    // given
-    val backendStub = TapirStubInterpreter(SttpBackendStub.synchronous)
-      .whenServerEndpointRunLogic(booksListingServerEndpoint)
-      .backend()
-
-    // when
-    val response = basicRequest
-      .get(uri"http://test.com/books/list/all")
-      .response(asJson[List[Book]])
-      .send(backendStub)
-
-    // then
-    response.body.value shouldBe books
+    response.body.value shouldBe "Hello world!"
   }
