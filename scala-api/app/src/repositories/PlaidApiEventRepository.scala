22f8b1cb63ed658f1f368859b5ae4e96756867ad
Dennis Dang <dangggdennis@gmail.com>
2024-06-24 15:24:16 -0700

    plaid api event repo

---
+++ scala-api/app/src/repositories/PlaidApiEventRepository.scala
@@ -0,0 +1,29 @@
+package app.repositories
+
+import scalikejdbc.*
+import io.circe.syntax.*
+import io.circe.generic.auto.*
+
+import java.util.UUID
+import scala.util.Try
+
+object PlaidApiEventRepository:
+  case class PlaidApiEventCreateInput(
+      userId: UUID,
+      itemId: UUID,
+      plaidMethod: String,
+      arguments: Map[String, String],
+      requestId: String,
+      errorType: Option[String],
+      errorCode: Option[String]
+  )
+
+  def create(input: PlaidApiEventCreateInput): Try[Unit] =
+    Try(DB autoCommit { implicit session =>
+      val query =
+        sql"""INSERT INTO plaid_api_events (item_id, user_id, plaid_method, arguments, request_id, error_type, error_code)
+        VALUES (${input.itemId}, ${input.userId}, ${input.plaidMethod}, ${input.arguments.asJson.noSpaces}, ${input.requestId}, ${input.errorType}, ${input.errorCode})
+        """
+      query.execute
+        .apply()
+    })
