2bc247e5837d0b49f9c45408428cb0875ebf3aa9
Dennis Dang <dangggdennis@gmail.com>
2024-07-03 03:05:07 -0400

    successfully open a plaid link

---
+++ mobile/lib/src/sample_feature/sample_item_details_view.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:plaid_flutter/plaid_flutter.dart';
+
+/// Displays detailed information about a SampleItem.
+class SampleItemDetailsView extends StatelessWidget {
+  const SampleItemDetailsView({super.key});
+
+  static const routeName = '/sample_item';
+
+  void _openPlaidLink() {
+    LinkConfiguration configuration = LinkTokenConfiguration(
+      token: "link-sandbox-6502787a-a144-4308-9256-a823e74d4da4",
+    );
+
+    PlaidLink.open(configuration: configuration);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Item Details'),
+      ),
+      body: Container(
+        child: Column(
+          children: [
+            Center(
+              child: ElevatedButton(
+                  onPressed: () {
+                    print("Button Pressed");
+                    _openPlaidLink();
+                  },
+                  child: const Text("Press Me")),
+            ),
+            const Center(
+              child: Text('More Information Here'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
