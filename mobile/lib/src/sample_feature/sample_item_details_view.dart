3ac179d5bdfd2b2fc6de944c7946319af28d4197
Dennis Dang <dangggdennis@gmail.com>
2024-07-04 02:14:24 -0400

    open plaid link modal

--- mobile/lib/src/sample_feature/sample_item_details_view.dart
+++ mobile/lib/src/sample_feature/sample_item_details_view.dart
@@ -1,5 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:plaid_flutter/plaid_flutter.dart';
+import 'package:http/http.dart' as http;
 
 /// Displays detailed information about a SampleItem.
 class SampleItemDetailsView extends StatelessWidget {
@@ -9,10 +12,52 @@ class SampleItemDetailsView extends StatelessWidget {
 
   void _openPlaidLink() {
     LinkConfiguration configuration = LinkTokenConfiguration(
-      token: "link-sandbox-6502787a-a144-4308-9256-a823e74d4da4",
+      token: "link-production-5fcef624-992a-4be7-b207-e6bda0ff1e7c",
     );
 
+    PlaidLink.onSuccess.listen((LinkSuccess success) async {
+      print("Success: ${success}");
+      await _sendPostRequest(success.publicToken);
+    });
+
+    PlaidLink.onExit.listen((LinkExit exit) {
+      // Handle the exit callback
+      print("User exited the Plaid Link flow");
+    });
+
+    PlaidLink.onEvent.listen((LinkEvent event) {
+      // Handle events (optional)
+      print('Event: ${event}');
+    });
+
     PlaidLink.open(configuration: configuration);
+  }
+
+  Future<void> _sendPostRequest(String publicToken) async {
+    final url =
+        Uri.parse('https://finny-backend.fly.dev/api/plaid-items/create');
+    final headers = {
+      'Content-Type': 'application/json',
+      'Authorization':
+          'Bearer eyJhbGciOiJIUzI1NiIsImtpZCI6IlFtdEZUekozSEd4T3ZzL1giLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzIwMTU1Mjk5LCJpYXQiOjE3MjAwNjg4OTksImlzcyI6Imh0dHBzOi8vdHFvbmt4aHJ1Y3ltZHluZHBqemYuc3VwYWJhc2UuY28vYXV0aC92MSIsInN1YiI6IjRjZTRiOTU4LWVhMWUtNDFlNi1iZDZhLWY4YzQzOTIyYTY5OSIsImVtYWlsIjoiZGFuZ2dnZGVubmlzQGdtYWlsLmNvbSIsInBob25lIjoiIiwiYXBwX21ldGFkYXRhIjp7InByb3ZpZGVyIjoiZW1haWwiLCJwcm92aWRlcnMiOlsiZW1haWwiXX0sInVzZXJfbWV0YWRhdGEiOnsiZW1haWwiOiJkYW5nZ2dkZW5uaXNAZ21haWwuY29tIiwiZW1haWxfdmVyaWZpZWQiOmZhbHNlLCJwaG9uZV92ZXJpZmllZCI6ZmFsc2UsInN1YiI6IjRjZTRiOTU4LWVhMWUtNDFlNi1iZDZhLWY4YzQzOTIyYTY5OSJ9LCJyb2xlIjoiYXV0aGVudGljYXRlZCIsImFhbCI6ImFhbDEiLCJhbXIiOlt7Im1ldGhvZCI6Im90cCIsInRpbWVzdGFtcCI6MTcyMDA2ODg5OX1dLCJzZXNzaW9uX2lkIjoiZDRlYzIzMDItZDdlYy00YTcwLThjMDMtMjZiYjYxZmRiZThlIiwiaXNfYW5vbnltb3VzIjpmYWxzZX0.Nqd4vBWccx1T5q928X566m3kj4zXhB2SdZgsBozsFE8'
+    };
+    final body = json.encode({
+      'publicToken': publicToken,
+    });
+
+    try {
+      final response = await http.post(url, headers: headers, body: body);
+      if (response.statusCode == 200) {
+        print('Success: ${response.body}');
+        // Handle success
+      } else {
+        print('Error: ${response.statusCode} ${response.body}');
+        // Handle error
+      }
+    } catch (e) {
+      print('Exception: $e');
+      // Handle exception
+    }
   }
 
   @override
@@ -26,11 +71,15 @@ class SampleItemDetailsView extends StatelessWidget {
           children: [
             Center(
               child: ElevatedButton(
-                  onPressed: _openPlaidLink,
-                  child: const Text("Press Me")),
+                  onPressed: _openPlaidLink, child: const Text("Press Me")),
             ),
             const Center(
-              child: Text('More Information Here'),
+              child: Column(
+                children: [
+                  Text('More Information Here'),
+                  Text('link-production-5fcef624-992a-4be7-b207-e6bda0ff1e7c')
+                ],
+              ),
             ),
           ],
         ),
