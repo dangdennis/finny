1cbda3595d23812f07bac57aecf730abe0da6638
Dennis Dang <dangggdennis@gmail.com>
2024-07-05 01:32:45 -0400

    [app] setup supabase and powersync

---
+++ mobile/lib/src/app_config.dart
@@ -0,0 +1,5 @@
+class AppConfig {
+  static const powersyncUrl = "https://powersync.supabase.co";
+  static const supabaseUrl = "https://tqonkxhrucymdyndpjzf.supabase.co";
+  static const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxb25reGhydWN5bWR5bmRwanpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYxNzk4MTAsImV4cCI6MjAzMTc1NTgxMH0.rGtsQPBxLHHatZaBs1JPxsp-E8chFQL11lFHfryKAxc";
+}
---
+++ mobile/lib/src/models/schema.dart
@@ -0,0 +1,38 @@
+import 'package:powersync/powersync.dart';
+
+const schema = Schema([
+  Table('accounts', [
+    Column.text('item_id'),
+    Column.text('user_id'),
+    Column.text('plaid_account_id'),
+    Column.text('name'),
+    Column.text('mask'),
+    Column.text('official_name'),
+    Column.real('current_balance'),
+    Column.real('available_balance'),
+    Column.text('iso_currency_code'),
+    Column.text('unofficial_currency_code'),
+    Column.text('type'),
+    Column.text('subtype'),
+    Column.text('created_at'),
+    Column.text('updated_at'),
+    Column.text('deleted_at')
+  ]),
+  Table('transactions', [
+    Column.text('account_id'),
+    Column.text('plaid_transaction_id'),
+    Column.text('category'),
+    Column.text('subcategory'),
+    Column.text('type'),
+    Column.text('name'),
+    Column.real('amount'),
+    Column.text('iso_currency_code'),
+    Column.text('unofficial_currency_code'),
+    Column.text('date'),
+    Column.integer('pending'),
+    Column.text('account_owner'),
+    Column.text('created_at'),
+    Column.text('updated_at'),
+    Column.text('deleted_at')
+  ])
+])
