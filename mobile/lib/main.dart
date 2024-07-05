fba770915fbea1052c9515bce5de735bf1775a80
Dennis Dang <dangggdennis@gmail.com>
2024-07-05 01:48:10 -0400

    setup deeplink for ios and android

--- mobile/lib/main.dart
+++ mobile/lib/main.dart
@@ -1,10 +1,32 @@
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
+import 'package:logging/logging.dart';
 
 import 'src/app.dart';
+import 'src/powersync.dart';
 import 'src/settings/settings_controller.dart';
 import 'src/settings/settings_service.dart';
 
 void main() async {
+  Logger.root.level = Level.INFO;
+  Logger.root.onRecord.listen((record) {
+    if (kDebugMode) {
+      print(
+          '[${record.loggerName}] ${record.level.name}: ${record.time}: ${record.message}');
+
+      if (record.error != null) {
+        print(record.error);
+      }
+      if (record.stackTrace != null) {
+        print(record.stackTrace);
+      }
+    }
+  });
+
+  WidgetsFlutterBinding
+      .ensureInitialized(); //required to get sqlite filepath from path_provider before UI has initialized
+  await openDatabase();
+
   // Set up the SettingsController, which will glue user settings to multiple
   // Flutter Widgets.
   final settingsController = SettingsController(SettingsService());
--- mobile/lib/src/app_config.dart
+++ mobile/lib/src/app_config.dart
@@ -1,5 +1,7 @@
 class AppConfig {
-  static const powersyncUrl = "https://powersync.supabase.co";
+  static const powersyncUrl =
+      "https://668757e925c6e47d23577e1f.powersync.journeyapps.com";
   static const supabaseUrl = "https://tqonkxhrucymdyndpjzf.supabase.co";
-  static const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxb25reGhydWN5bWR5bmRwanpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYxNzk4MTAsImV4cCI6MjAzMTc1NTgxMH0.rGtsQPBxLHHatZaBs1JPxsp-E8chFQL11lFHfryKAxc";
+  static const supabaseAnonKey =
+      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRxb25reGhydWN5bWR5bmRwanpmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MTYxNzk4MTAsImV4cCI6MjAzMTc1NTgxMH0.rGtsQPBxLHHatZaBs1JPxsp-E8chFQL11lFHfryKAxc";
 }
