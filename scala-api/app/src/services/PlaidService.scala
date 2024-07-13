4818da08972a1298e823520c5258c3a21b2e5be0
Dennis Dang <dangggdennis@gmail.com>
2024-07-13 17:07:24 -0400

    [api] explicitly set all account subtypes

--- scala-api/app/src/services/PlaidService.scala
+++ scala-api/app/src/services/PlaidService.scala
@@ -32,6 +32,15 @@ import scala.reflect.ClassTag
 import scala.util.Failure
 import scala.util.Success
 import scala.util.Try
+import com.plaid.client.model.LinkTokenAccountFilters
+import com.plaid.client.model.CreditFilter
+import com.plaid.client.model.CreditAccountSubtype
+import com.plaid.client.model.DepositoryFilter
+import com.plaid.client.model.DepositoryAccountSubtype
+import com.plaid.client.model.InvestmentFilter
+import com.plaid.client.model.InvestmentAccountSubtype
+import com.plaid.client.model.LoanFilter
+import com.plaid.client.model.LoanAccountSubtype
 
 object PlaidService:
     private lazy val client = makePlaidClient()
@@ -95,7 +104,7 @@ object PlaidService:
                 List(
                     Products.TRANSACTIONS,
                     Products.INVESTMENTS,
-                    Products.LIABILITIES,
+                    Products.LIABILITIES
                     // Products.RECURRING_TRANSACTIONS
                 ).asJava
             )
@@ -106,6 +115,13 @@ object PlaidService:
             .clientName("Finny")
             .transactions(LinkTokenTransactions().daysRequested(360))
             .redirectUri(f"${Environment.getBaseUrl}/oauth/plaid")
+            .accountFilters(
+                LinkTokenAccountFilters()
+                    .credit(CreditFilter().accountSubtypes(List(CreditAccountSubtype.ALL).asJava))
+                    .depository(DepositoryFilter().accountSubtypes(List(DepositoryAccountSubtype.ALL).asJava))
+                    .investment(InvestmentFilter().accountSubtypes(List(InvestmentAccountSubtype.ALL).asJava))
+                    .loan(LoanFilter().accountSubtypes(List(LoanAccountSubtype.ALL).asJava))
+            )
 
         handleResponse(
             Try(client.linkTokenCreate(req).execute()),
