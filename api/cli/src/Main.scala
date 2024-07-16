2c3023a1f2eadfd23dcc3b726276ca4c304b8178
Dennis Dang <dangggdennis@gmail.com>
2024-07-16 14:50:50 -0400

    refactor cli

--- api/cli/src/Main.scala
+++ api/cli/src/Main.scala
@@ -9,33 +9,30 @@ import java.util.UUID
 
 object Cli {
     @main def main(args: String*): Unit =
+        val config = Environment.DatabaseConfig(
+            host = "jdbc:postgresql://aws-0-us-east-1.pooler.supabase.com:6543/postgres",
+            user = "postgres.tqonkxhrucymdyndpjzf",
+            password = "I07R6V4POCTi5wd4"
+        )
+        Database.init(config)
+        println("Database initialized")
+        val plaidClient = PlaidService.makePlaidClient(
+            clientId = "661ac9375307a3001ba2ea46",
+            secret = "b7cdf3790e64dffb46b4edd1762f70",
+            env = AppEnv.Production
+        )
+
         args match
             case Seq() => println("Please provide a command")
             case command :: itemId :: _ =>
                 command match
                     case "delete-item" =>
-                        val config = Environment.DatabaseConfig(
-                            host = "jdbc:postgresql://aws-0-us-east-1.pooler.supabase.com:6543/postgres",
-                            user = "postgres.tqonkxhrucymdyndpjzf",
-                            password = "I07R6V4POCTi5wd4"
-                        )
-
-                        val results = for
-                            _ <- Database.init(config)
-                            _ = println("Database initialized")
-                            res <- PlaidService.removeItem(
-                                client = PlaidService.makePlaidClient(
-                                    clientId = "661ac9375307a3001ba2ea46",
-                                    secret = "b7cdf3790e64dffb46b4edd1762f70",
-                                    env = AppEnv.Production
-                                ),
-                                itemId = UUID.fromString(itemId)
-                            )
-                        yield res
-
+                        val results =
+                            for res <- PlaidService.removeItem(
+                                    client = plaidClient,
+                                    itemId = UUID.fromString(itemId)
+                                )
+                            yield res
                         println(results)
-
-                    case "update-all-plaid-items-webhook" =>
-                        println("Goodbye, world!")
                     case _ => println("Unknown command")
 }
