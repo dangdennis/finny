204f4353e782dcdf926cfbd195d42438d94b945d
Dennis Dang <dangggdennis@gmail.com>
2024-07-25 15:31:19 -0400

    fix migraiton

--- api/supabase/migrations/20240725050100_investment_holdings_and_transactions.sql
+++ api/supabase/migrations/20240725050100_investment_holdings_and_transactions.sql
@@ -1,5 +1,31 @@
 ALTER TABLE plaid_items
-	ADD columns error_type text, error_code text, error_message text, error_display_message text, error_request_id text, documentation_url text, suggested_action text;
+	ADD COLUMN error_type text,
+	ADD COLUMN error_code varchar,
+	ADD COLUMN error_message varchar,
+	ADD COLUMN error_display_message varchar,
+	ADD COLUMN error_request_id varchar,
+	ADD COLUMN documentation_url varchar,
+	ADD COLUMN suggested_action varchar;
+
+
+	CREATE TABLE IF NOT EXISTS investment_securities (
+		id uuid PRIMARY KEY DEFAULT gen_random_uuid (),
+		plaid_security_id varchar NOT NULL,
+		plaid_institution_security_id varchar,
+		plaid_institution_id varchar,
+		plaid_proxy_security_id varchar,
+		name varchar,
+		ticker_symbol varchar,
+		security_type varchar,
+		created_at timestamptz NOT NULL,
+		updated_at timestamptz NOT NULL,
+		deleted_at timestamptz
+);
+
+CREATE INDEX ix_investment_securities_plaid_security_id ON investment_securities(plaid_security_id);
+CREATE INDEX ix_investment_securities_plaid_institution_security_id ON investment_securities(plaid_institution_security_id);
+CREATE INDEX ix_investment_securities_plaid_institution_id ON investment_securities(plaid_institution_id);
+CREATE INDEX ix_investment_securities_plaid_proxy_security_id ON investment_securities(plaid_proxy_security_id);
 
 CREATE TABLE IF NOT EXISTS investment_transactions (
 	id uuid PRIMARY KEY DEFAULT gen_random_uuid (),
@@ -20,15 +46,14 @@ CREATE TABLE IF NOT EXISTS investment_transactions (
 	deleted_at timestamptz
 );
 
-CREATE INDEX ix:investment_transactions.plaid_investment_transaction_id ON investment_transactions(plaid_investment_transaction_id);
-CREATE INDEX ix:investment_transactions.account_id ON investment_transactions(account_id);
-CREATE INDEX ix:investment_transactions.investment_security_id ON investment_transactions(investment_security_id);
-
+CREATE INDEX ix_investment_transactions_plaid_investment_transaction_id ON investment_transactions(plaid_investment_transaction_id);
+CREATE INDEX ix_investment_transactions_account_id ON investment_transactions(account_id);
+CREATE INDEX ix_investment_transactions_investment_security_id ON investment_transactions(investment_security_id);
 
 CREATE TABLE IF NOT EXISTS investment_holdings (
 	id uuid PRIMARY KEY DEFAULT gen_random_uuid (),
 	account_id uuid NOT NULL REFERENCES accounts (id),
-	security_id varchar NOT NULL REFERENCES investment_securities (id),
+	investment_security_id uuid NOT NULL REFERENCES investment_securities (id),
 	institution_price double precision NOT NULL,
 	institution_price_as_of timestamptz,
 	institution_price_date_time timestamptz,
@@ -43,24 +68,5 @@ CREATE TABLE IF NOT EXISTS investment_holdings (
 	deleted_at timestamptz
 );
 
-CREATE INDEX ix:investment_holdings.account_id ON investment_holdings(account_id);
-CREATE INDEX ix:investment_holdings.security_id ON investment_holdings(security_id);
-
-CREATE TABLE IF NOT EXISTS investment_securities (
-	id uuid PRIMARY KEY DEFAULT gen_random_uuid (),
-	plaid_security_id varchar NOT NULL,
-	plaid_institution_security_id varchar,
-	plaid_institution_id varchar,
-	plaid_proxy_security_id varchar,
-	name varchar,
-	ticker_symbol varchar,
-	security_type varchar,
-	created_at timestamptz NOT NULL,
-	updated_at timestamptz NOT NULL,
-	deleted_at timestamptz
-);
-
-CREATE INDEX ix:investment_securities.plaid_security_id ON investment_securities(plaid_security_id);
-CREATE INDEX ix:investment_securities.plaid_institution_security_id ON investment_securities(plaid_institution_security_id);
-CREATE INDEX ix:investment_securities.plaid_institution_id ON investment_securities(plaid_institution_id);
-CREATE INDEX ix:investment_securities.plaid_proxy_security_id ON investment_securities(plaid_proxy_security_id);
+CREATE INDEX ix_investment_holdings_account_id ON investment_holdings(account_id);
+CREATE INDEX ix_investment_holdings_investment_security_id ON investment_holdings(investment_security_id);
