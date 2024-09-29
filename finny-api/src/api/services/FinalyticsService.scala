95ab200221c22dfbe6372541e35255b77f28cc80
Dennis Dang <dangggdennis@gmail.com>
2024-09-29 14:36:26 -0700

    setup scalasql"

--- finny-api/src/api/services/FinalyticsService.scala
+++ finny-api/src/api/services/FinalyticsService.scala
@@ -1,65 +1,61 @@
 package api.services
 
 import api.common.*
-import scalikejdbc.*
 
 import java.util.UUID
 import scala.util.Try
+import scalasql.*, PostgresDialect.*
+import scalasql.core.*
+import scalasql.core.SqlStr.*
 
 object FinalyticsService:
   def calculateRetirementSavingsForCurrentMonth(
       userId: UUID
-  ): Either[AppError, Double] =
-    val result = Try:
-      DB.readOnly:
-        implicit session =>
-          sql"${retirementSavingsQuery(userId)}"
-            .map(rs => rs.doubleOpt("net_balance_change"))
-            .single
-            .apply()
-            .map(_.getOrElse(0.0))
-            
+  )(using dbClient: DbClient.DataSource): Either[AppError, Double] =
+    val result =
+      Try:
+        dbClient.transaction: db =>
+          db.runSql[Double](sql"""
+            WITH retirement_goal AS (
+              SELECT id
+              FROM goals
+              WHERE goals.goal_type = 'retirement' AND goals.user_id = $userId
+              LIMIT 1
+            ),
+            assigned_accounts AS (
+              SELECT account_id
+              FROM goal_accounts
+              JOIN retirement_goal ON retirement_goal.id = goal_accounts.goal_id
+            ),
+            start_of_month_balances AS (
+              -- Get the earliest balance in the current month for each account
+              SELECT DISTINCT ON (ab.account_id)
+                ab.account_id,
+                ab.balance_date AS start_balance_date,
+                ab.current_balance AS start_balance
+              FROM account_balances ab
+              WHERE ab.balance_date >= date_trunc('month', CURRENT_DATE)
+                AND ab.account_id IN (SELECT account_id FROM assigned_accounts)
+              ORDER BY ab.account_id, ab.balance_date
+            ),
+            most_recent_balances AS (
+              -- Get the most recent balance for each account
+              SELECT DISTINCT ON (ab.account_id)
+                ab.account_id,
+                ab.balance_date AS most_recent_balance_date,
+                ab.current_balance AS most_recent_balance
+              FROM account_balances ab
+              WHERE ab.account_id IN (SELECT account_id FROM assigned_accounts)
+              ORDER BY ab.account_id, ab.balance_date DESC
+            )
+            SELECT
+              SUM(mr.most_recent_balance - som.start_balance) AS net_balance_change
+            FROM most_recent_balances mr
+            JOIN start_of_month_balances som ON mr.account_id = som.account_id
+            JOIN accounts ON mr.account_id = accounts.id
+            WHERE mr.most_recent_balance_date >= som.start_balance_date
+          """)
 
-    result.toEither.left.map(e => AppError.DatabaseError(e.getMessage)).map(_.getOrElse(0.0))
-
-  private def retirementSavingsQuery(userId: UUID): SQL[Nothing, NoExtractor] =
-    sql"""
-      WITH retirement_goal AS (
-        SELECT id
-        FROM goals
-        WHERE goals.goal_type = 'retirement' AND goals.user_id = ${userId.toString()}
-        LIMIT 1
-      ),
-      assigned_accounts AS (
-        SELECT account_id
-        FROM goal_accounts
-        JOIN retirement_goal ON retirement_goal.id = goal_accounts.goal_id
-      ),
-      start_of_month_balances AS (
-        -- Get the earliest balance in the current month for each account
-        SELECT DISTINCT ON (ab.account_id)
-          ab.account_id,
-          ab.balance_date AS start_balance_date,
-          ab.current_balance AS start_balance
-        FROM account_balances ab
-        WHERE ab.balance_date >= date_trunc('month', CURRENT_DATE)
-          AND ab.account_id IN (SELECT account_id FROM assigned_accounts)
-        ORDER BY ab.account_id, ab.balance_date
-      ),
-      most_recent_balances AS (
-        -- Get the most recent balance for each account
-        SELECT DISTINCT ON (ab.account_id)
-          ab.account_id,
-          ab.balance_date AS most_recent_balance_date,
-          ab.current_balance AS most_recent_balance
-        FROM account_balances ab
-        WHERE ab.account_id IN (SELECT account_id FROM assigned_accounts)
-        ORDER BY ab.account_id, ab.balance_date DESC
-      )
-      SELECT
-        SUM(mr.most_recent_balance - som.start_balance) AS net_balance_change
-      FROM most_recent_balances mr
-      JOIN start_of_month_balances som ON mr.account_id = som.account_id
-      JOIN accounts ON mr.account_id = accounts.id
-      WHERE mr.most_recent_balance_date >= som.start_balance_date
-    """
+    result.toEither.left
+      .map(e => AppError.DatabaseError(e.getMessage))
+      .map(rs => rs.headOption.getOrElse(0.0))
--- finny-api/src/test/services/FinalyticsServiceSpec.scala
+++ finny-api/src/test/services/FinalyticsServiceSpec.scala
@@ -3,14 +3,17 @@ package test.services
 import api.common.Environment.DatabaseConfig
 import api.repositories.TransactionRepository
 import org.scalatest.flatspec.AnyFlatSpec
-import test.helpers.*
-
 import java.util.UUID
 import org.scalatest.matchers.should.Matchers
 import org.scalatest.EitherValues
 import org.scalatest.BeforeAndAfterAll
 import org.scalatest.BeforeAndAfterEach
 import api.services.FinalyticsService
+import api.database.DatabaseScalaSql
+import scalasql.*, PostgresDialect.*
+import scalasql.core.*
+import scalasql.core.SqlStr.*
+import scalasql.core.DbClient.DataSource
 
 // To run this test, run `docker compose up` and then `make restore-prod-db`.
 // This ensures we have a database with prod data.
@@ -22,27 +25,37 @@ class FinalyticsServiceSpec
       BeforeAndAfterAll,
       BeforeAndAfterEach:
 
-  override protected def beforeAll(): Unit =
-    FinalyticsDatabase.init(
-      DatabaseConfig(
-        host = "jdbc:postgresql://localhost:5432/postgres",
-        user = "postgres",
-        password = "postgres"
-      )
-    )
+  var dbClient: DataSource = scala.compiletime.uninitialized
+  given DataSource = dbClient
 
-  "We" should "be able to query transactions from the prod dump db" in:
-    val transactions = TransactionRepository
-      .getTransactionsByAccountId(
-        UUID.fromString("495f3ee1-6187-4dcb-98a4-f6fa4b01b0b9")
+  override protected def beforeAll(): Unit = {
+    dbClient = DatabaseScalaSql
+      .init(
+        DatabaseConfig(
+          host = "jdbc:postgresql://localhost:5432/postgres",
+          user = "postgres",
+          password = "postgres"
+        )
       )
       .value
-    assert(transactions.length == 337)
+  }
+
+  "Using scalasql" should "be able to query transactions from the prod dump db" in:
+    val userId = UUID.fromString("5eaa8ae7-dbcb-445e-8058-dbd51a912c8d")
+    val accountId = UUID.fromString("495f3ee1-6187-4dcb-98a4-f6fa4b01b0b9")
+
+    val results = dbClient.transaction: db =>
+      db.runSql[Double](
+        sql"""select count(*) from transactions where account_id = ${accountId}"""
+      )
+
+    assert(results(0) == 337)
 
   "calculateRetirementSavingsForCurrentMonth" should "return the correct retirement savings for the current month" in:
-    // given
-    // hardcoded to dennis user id because we use a dump of the prod database
+
     val userId = UUID.fromString("5eaa8ae7-dbcb-445e-8058-dbd51a912c8d")
-    val result = FinalyticsService.calculateRetirementSavingsForCurrentMonth(userId)
+    val result =
+      FinalyticsService
+        .calculateRetirementSavingsForCurrentMonth(userId)
     assert(result.isRight)
-    assert(result.value == 1000.0)
+    assert(result.value == -7238.82)
