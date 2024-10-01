81a2b8f977b8a516b6466a2618aeaec82a5e9826
Dennis Dang <dangggdennis@gmail.com>
2024-10-01 01:56:24 -0700

    delete tauri

--- archive/tauri/backend/src/log.rs
+++
@@ -1,8 +0,0 @@
-use tracing_subscriber::{filter::LevelFilter, fmt};
-
-pub fn init_tracing() {
-    fmt()
-        .with_max_level(LevelFilter::DEBUG)
-        .with_test_writer()
-        .init();
-}
