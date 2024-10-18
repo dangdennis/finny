0cf2e5405a7543c0aeaf3bb9c5e8a600733d9abd
Dennis Dang <dangggdennis@gmail.com>
2024-10-17 21:35:07 -0400

    update copy

--- marketing/src/components/Hero.tsx
+++ marketing/src/components/Hero.tsx
@@ -108,8 +108,9 @@ export function Hero() {
               Know financial freedom.
             </h1>
             <p className="mt-6 text-lg text-gray-600">
-              By leveraging real-time insights on your finances, you&apos;ll
-              know exactly how much to save and choose your retirement day.
+              Connect your YNAB account to leverage real-time insights on your
+              finances. Choose your freedom date and know exactly how much
+              you&apos;ll need to save for freedom.
             </p>
             <div className="mt-8 flex flex-wrap gap-x-6 gap-y-4">
               <AppStoreLink />
