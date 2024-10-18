1cadfe43f77f4ca6df02fdf6ec81e60b0d0855a9
Dennis Dang <dangggdennis@gmail.com>
2024-10-18 16:37:00 -0400

    create methods to recompute account balances based on transactions

--- finny-worker/finalytics/finalytics_test.go
+++ finny-worker/finalytics/finalytics_test.go
@@ -2,23 +2,35 @@ package finalytics
 
 import (
 	"testing"
+	"time"
 
 	"github.com/finny/worker/account"
 	"github.com/finny/worker/database"
 	"github.com/finny/worker/goal"
 	"github.com/finny/worker/profile"
 	"github.com/finny/worker/transaction"
+	"github.com/google/uuid"
 	"github.com/stretchr/testify/require"
 )
 
 func TestFinalyticsService(t *testing.T) {
-	db, err := database.NewTestDatabase()
+	testDB, err := database.NewTestDatabase()
 	require.NoError(t, err, "Failed to connect to database")
-	accountRepo := account.NewAccountRepository(db)
-	profileRepo := profile.NewProfileRepository(db)
-	goalRepo := goal.NewGoalRepository(db, accountRepo)
-	transactionRepo := transaction.NewTransactionRepository(db)
-	finSvc := NewFinalyticsService(db, profileRepo, goalRepo, transactionRepo)
+
+	testCalcDB, err := database.NewTestCalcDatabase()
+	require.NoError(t, err, "Failed to connect to database")
+
+	accountRepo := account.NewAccountRepository(testDB)
+	profileRepo := profile.NewProfileRepository(testDB)
+	goalRepo := goal.NewGoalRepository(testDB, accountRepo)
+	transactionRepo := transaction.NewTransactionRepository(testDB)
+	finSvcWithTestDB := NewFinalyticsService(testDB, accountRepo, profileRepo, goalRepo, transactionRepo)
+
+	accountRepo = account.NewAccountRepository(testCalcDB)
+	profileRepo = profile.NewProfileRepository(testCalcDB)
+	goalRepo = goal.NewGoalRepository(testCalcDB, accountRepo)
+	transactionRepo = transaction.NewTransactionRepository(testCalcDB)
+	finSvcWithTestCalcDB := NewFinalyticsService(testCalcDB, accountRepo, profileRepo, goalRepo, transactionRepo)
 
 	t.Run("Pmt", func(t *testing.T) {
 		tc := []struct {
@@ -45,7 +57,7 @@ func TestFinalyticsService(t *testing.T) {
 		}
 
 		for _, tc := range tc {
-			pmt, err := finSvc.Pmt(PmtInput{
+			pmt, err := finSvcWithTestDB.Pmt(PmtInput{
 				rate:       tc.Rate,
 				numPeriods: tc.NumPeriods,
 				pv:         tc.Pv,
@@ -82,7 +94,7 @@ func TestFinalyticsService(t *testing.T) {
 			}
 
 			for _, tc := range tc {
-				pmt, err := finSvc.Nper(NperInput{
+				pmt, err := finSvcWithTestDB.Nper(NperInput{
 					rate: tc.Rate,
 					pmt:  tc.Pmt,
 					pv:   tc.Pv,
@@ -118,7 +130,7 @@ func TestFinalyticsService(t *testing.T) {
 			}
 
 			for _, tc := range tc {
-				_, err := finSvc.Nper(NperInput{
+				_, err := finSvcWithTestDB.Nper(NperInput{
 					rate: tc.Rate,
 					pmt:  tc.Pmt,
 					pv:   tc.Pv,
@@ -156,7 +168,7 @@ func TestFinalyticsService(t *testing.T) {
 			}
 
 			for _, tc := range tc {
-				fv, err := finSvc.Fv(FvInput{
+				fv, err := finSvcWithTestDB.Fv(FvInput{
 					rate:       tc.Rate,
 					pmt:        tc.Pmt,
 					pv:         tc.Pv,
@@ -167,5 +179,96 @@ func TestFinalyticsService(t *testing.T) {
 			}
 		})
 
+	})
+
+	t.Run("ComputeAccountBalance", func(t *testing.T) {
+		t.Run("Compute without persisting", func(t *testing.T) {
+			startDateInclusive := time.Date(2024, time.September, 18, 0, 0, 0, 0, time.UTC)
+			endDateExclusive := time.Date(2024, time.October, 5, 0, 0, 0, 0, time.UTC)
+			balances, err := finSvcWithTestCalcDB.ComputeAccountBalance(ComputeAccountBalanceInput{
+				AccountID:          uuid.MustParse("b80a370f-f36c-45d8-853e-3ddf35557816"),
+				StartDateInclusive: startDateInclusive,
+				EndDateInclusive:   endDateExclusive,
+			})
+			require.NoError(t, err)
+			require.Equal(t, 18, len(balances.Balances))
+
+			expectedBalances := []struct {
+				Date    time.Time
+				Balance float64
+			}{
+				{time.Date(2024, 10, 5, 0, 0, 0, 0, time.UTC), 589.62},
+				{time.Date(2024, 10, 4, 0, 0, 0, 0, time.UTC), 459.20000000000005},
+				{time.Date(2024, 10, 3, 0, 0, 0, 0, time.UTC), 291.47},
+				{time.Date(2024, 10, 2, 0, 0, 0, 0, time.UTC), 196.24},
+				{time.Date(2024, 10, 1, 0, 0, 0, 0, time.UTC), 196.24},
+				{time.Date(2024, 9, 30, 0, 0, 0, 0, time.UTC), -189.29000000000002},
+				{time.Date(2024, 9, 29, 0, 0, 0, 0, time.UTC), -189.29000000000002},
+				{time.Date(2024, 9, 28, 0, 0, 0, 0, time.UTC), -189.29000000000002},
+				{time.Date(2024, 9, 27, 0, 0, 0, 0, time.UTC), 504},
+				{time.Date(2024, 9, 26, 0, 0, 0, 0, time.UTC), 407},
+				{time.Date(2024, 9, 25, 0, 0, 0, 0, time.UTC), 407},
+				{time.Date(2024, 9, 24, 0, 0, 0, 0, time.UTC), 3331.83},
+				{time.Date(2024, 9, 23, 0, 0, 0, 0, time.UTC), 3074.0599999999995},
+				{time.Date(2024, 9, 22, 0, 0, 0, 0, time.UTC), 3074.0599999999995},
+				{time.Date(2024, 9, 21, 0, 0, 0, 0, time.UTC), 3074.0599999999995},
+				{time.Date(2024, 9, 20, 0, 0, 0, 0, time.UTC), 2983.7999999999993},
+				{time.Date(2024, 9, 19, 0, 0, 0, 0, time.UTC), 2671.0299999999997},
+				{time.Date(2024, 9, 18, 0, 0, 0, 0, time.UTC), 2655.7799999999997},
+			}
+
+			require.Equal(t, len(expectedBalances), len(balances.Balances))
+
+			for i, expected := range expectedBalances {
+				require.Equal(t, expected.Date, balances.Balances[i].Date)
+				require.InDelta(t, expected.Balance, balances.Balances[i].CurrentBal, 0.000001)
+			}
+		})
+
+		t.Run("Compute with persisting", func(t *testing.T) {
+			startDateInclusive := time.Date(2024, time.September, 18, 0, 0, 0, 0, time.UTC)
+			endDateExclusive := time.Date(2024, time.October, 5, 0, 0, 0, 0, time.UTC)
+			acctID := uuid.MustParse("b80a370f-f36c-45d8-853e-3ddf35557816")
+			balances, err := finSvcWithTestCalcDB.ComputeAccountBalance(ComputeAccountBalanceInput{
+				AccountID:          acctID,
+				StartDateInclusive: startDateInclusive,
+				EndDateInclusive:   endDateExclusive,
+				Persist:            true,
+			})
+			require.NoError(t, err)
+			require.Equal(t, 18, len(balances.Balances))
+
+			expectedBalances := []struct {
+				Date    time.Time
+				Balance float64
+			}{
+				{time.Date(2024, 10, 5, 0, 0, 0, 0, time.UTC), 589.62},
+				{time.Date(2024, 10, 4, 0, 0, 0, 0, time.UTC), 459.20000000000005},
+				{time.Date(2024, 10, 3, 0, 0, 0, 0, time.UTC), 291.47},
+				{time.Date(2024, 10, 2, 0, 0, 0, 0, time.UTC), 196.24},
+				{time.Date(2024, 10, 1, 0, 0, 0, 0, time.UTC), 196.24},
+				{time.Date(2024, 9, 30, 0, 0, 0, 0, time.UTC), -189.29000000000002},
+				{time.Date(2024, 9, 29, 0, 0, 0, 0, time.UTC), -189.29000000000002},
+				{time.Date(2024, 9, 28, 0, 0, 0, 0, time.UTC), -189.29000000000002},
+				{time.Date(2024, 9, 27, 0, 0, 0, 0, time.UTC), 504},
+				{time.Date(2024, 9, 26, 0, 0, 0, 0, time.UTC), 407},
+				{time.Date(2024, 9, 25, 0, 0, 0, 0, time.UTC), 407},
+				{time.Date(2024, 9, 24, 0, 0, 0, 0, time.UTC), 3331.83},
+				{time.Date(2024, 9, 23, 0, 0, 0, 0, time.UTC), 3074.0599999999995},
+				{time.Date(2024, 9, 22, 0, 0, 0, 0, time.UTC), 3074.0599999999995},
+				{time.Date(2024, 9, 21, 0, 0, 0, 0, time.UTC), 3074.0599999999995},
+				{time.Date(2024, 9, 20, 0, 0, 0, 0, time.UTC), 2983.7999999999993},
+				{time.Date(2024, 9, 19, 0, 0, 0, 0, time.UTC), 2671.0299999999997},
+				{time.Date(2024, 9, 18, 0, 0, 0, 0, time.UTC), 2655.7799999999997},
+			}
+
+			gotBalances, err := finSvcWithTestCalcDB.accountRepo.GetAccountBalancesInDateRange(acctID, startDateInclusive, endDateExclusive)
+			require.NoError(t, err)
+			require.Equal(t, len(expectedBalances), len(gotBalances))
+			for i, expected := range expectedBalances {
+				require.Equal(t, expected.Date, gotBalances[i].BalanceDate)
+				require.InDelta(t, expected.Balance, gotBalances[i].CurrentBalance, 0.000001)
+			}
+		})
 	})
 }
---
+++ supabase/migrations/20241018203406_unique_index_on_account_balances.sql
@@ -0,0 +1,1 @@
+CREATE UNIQUE INDEX idx_account_balances_account_date ON account_balances(account_id, balance_date);
