cf9b7b641a0ccb6d722dc6a8c65f35a18d7b298d
Dennis Dang <dangggdennis@gmail.com>
2024-11-04 19:36:22 -0500

    rename all folders

---
+++ ios/FinnyUITests/FinnyUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  FinnyUITestsLaunchTests.swift
+//  FinnyUITests
+//
+//  Created by Dennis Dang on 11/4/24.
+//
+
+import XCTest
+
+final class FinnyUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
