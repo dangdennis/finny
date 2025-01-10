7c606122906620e098fafca00e85f287e66de085
Rani Bungay <ranibungay@gmail.com>
2025-01-10 12:44:56 -0800

    create tabs on the bottom separate for Dashboard and for Settings

--- finny-app/lib/src/views/dashboard/dashboard_view.dart
+++ finny-app/lib/src/views/dashboard/dashboard_view.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:finny/src/views/settings/settings_view.dart';
-import 'package:finny/src/settings/settings_controller.dart';
 
 class DashboardView extends StatefulWidget {
   const DashboardView({
@@ -12,15 +11,46 @@ class DashboardView extends StatefulWidget {
 }
 
 class _DashboardViewState extends State<DashboardView> {
+  int _selectedIndex = 0;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(title: const Text('Dashboard')),
-        body: SingleChildScrollView(
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [Text("Dashboard")],
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: [
+          Scaffold(
+            appBar: AppBar(title: const Text('Dashboard')),
+            body: const SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [Text('Dashboard')],
+              ),
+            ),
           ),
-        ));
+          const SettingsView(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _selectedIndex,
+        onTap: (index) {
+          setState(() {
+            _selectedIndex = index;
+          });
+        },
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons
+                .dashboard), // Changed from Icon.settings to Icons.dashboard
+            label: 'Dashboard',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+                Icons.settings), // Changed from Icon.settings to Icons.settings
+            label: 'Settings',
+          ),
+        ],
+      ),
+    );
   }
 }
