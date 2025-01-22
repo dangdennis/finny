7dd6187a6474f892602d29717341239b6e051f26
Rani Bungay <ranibungay@gmail.com>
2025-01-21 18:35:48 -0800

    resolve merge conflicts and make callback for ynab work

--- finny-backend/bin/server/server.go
+++ finny-backend/bin/server/server.go
@@ -4,45 +4,4 @@ import "github.com/finny/finny-backend/internal/app"
 
 func StartServer() {
 	app.StartServer()
-	err := godotenv.Load(".env")
-	if err != nil {
-		log.Println("Error loading .env file:", err)
-	} else {
-		log.Println(".env file loaded successfully")
-	}
-
-	databaseUrl := os.Getenv("DATABASE_URL")
-	if databaseUrl == "" {
-		log.Fatal("DATABASE_URL is not set")
-	}
-
-	db, err := database.NewDatabase(databaseUrl)
-	if err != nil {
-		log.Fatalf("Failed to connect to database: %v", err)
-	}
-
-	budgetService := budget.NewBudgetService(db)
-	ynabAuthService := ynab_auth.NewYNABAuthService()
-
-	budgetController := controllers.NewBudgetController(budgetService)
-	ynabOAuthController := controllers.NewYNABController(ynabAuthService)
-
-	http.HandleFunc("POST /api/get-expense", func(w http.ResponseWriter, r *http.Request) {
-		budgetController.GetExpense(w, r)
-	})
-
-	http.HandleFunc("POST /api/oauth/ynab/whatever", func(w http.ResponseWriter, r *http.Request) {
-		ynabOAuthController.SomeRouteHandler(w, r)
-	})
-
-	http.HandleFunc("GET /api/oauth/ynab/authorize", func(w http.ResponseWriter, r *http.Request) {
-		ynabOAuthController.InitiateOAuth(w, r)
-	})
-
-	http.HandleFunc("GET /api/oauth/ynab/callback", func(w http.ResponseWriter, r *http.Request) {
-		ynabOAuthController.HandleCallback(w, r)
-	})
-
-	log.Println("Finny server is listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
 }
